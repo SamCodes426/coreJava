663bcb4d6f72390cda1a192032614c117229185f
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-05 13:46:15 +0530

    relationalOperators

---
+++ core java/relationalOperator.java
@@ -0,0 +1,20 @@
+
+class relationalOperator{
+    public static void main(String[] args) {
+        
+
+        System.out.println(3>1); // true
+
+        System.out.println(5<4); //false
+
+        System.out.println(12>=6); //true
+
+        System.out.println( 15<=10); //false
+
+        System.out.println(5==5); //true
+
+        System.out.println(5!=5); //false
+
+        System.out.println('A' < 'B'); //true
+    }
+}

c622e617ec3e6d25271d39a8d5edf41b865e27d5
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-05 13:44:15 +0530

    arithmaticOperators

---
+++ core java/arithmaticOperators.java
@@ -0,0 +1,40 @@
+public class arithmaticOperators {
+    public static void main(String[] args) {
+        
+        System.out.println(2+3); //5
+        
+        System.out.println(2-3); //-1
+        
+        System.out.println(2*3); //6
+
+        System.out.println(17/5); //3
+        
+        System.out.println(17%5); //2
+
+        System.out.println(17.0/5); //3.4
+
+        System.out.println(17.0%5); //2.0
+    
+        System.out.println(15/5.0); //3.0
+
+        System.out.println(1/2*20*30); //0
+
+        System.out.println('A'+10);   //65+10 = 75
+        
+        System.out.println('A'+'B'); //65+66 = 131
+        
+        //System.out.println(true+10);  
+        
+//============================ CONCATENATION =================================//
+        
+        System.out.println("Hello"+10);  //concatenation occurs when atleast one operand is string 
+    
+        System.out.println("Hello"+10+20); //Hello1020
+    
+        System.out.println("Hello"+(10+20)); //Hello30
+
+        System.out.println(10+20+"Hello"); //30Hello
+
+        System.out.println("Hand"+"some"); //Handsome
+    }
+}

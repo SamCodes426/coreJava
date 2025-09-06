ba7bc8be5b439426a214c5a8e22ab9f16cbd3767
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-06 13:12:19 +0530

    conditionalOperator

---
+++ core java/conditionalOperator.java
@@ -0,0 +1,11 @@
+public class conditionalOperator {
+    public static void main(String[] args) {
+        
+
+        int age = 16;
+
+        String x = (age>=18) ? "you are eligible to drink" : "you can not drink this ";
+
+        System.out.println(x);
+    }
+}

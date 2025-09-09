dbf2e26a62b559cb1dddb8f6f3ff992a5ec550c0
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-09 11:55:27 +0530

    combinationalOperator

---
+++ core java/combinationalOperator.java
@@ -0,0 +1,27 @@
+
+// ============================ COMBINATIONAL OPERATORS ==================================
+
+public class combinationalOperator {
+    public static void main(String[] args) {
+        
+        int a = 5;
+        a+=3;
+        System.out.println(a);
+
+        int b = 5;
+        b -= 2;
+        System.out.println(b);
+
+        int c = 5;
+        c *= 3;
+        System.out.println(c);
+
+        int d = 17;
+        d /= 3;
+        System.out.println(d);
+
+        int e = 17;
+        e %= 3;
+        System.out.println(e);
+    }
+}

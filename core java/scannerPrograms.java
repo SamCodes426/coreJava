8ca1ee3ebe8f16ce886e9dacdee4adf4bcae19fd
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-09 12:06:42 +0530

    learn how to take input from user using scanner

---
+++ core java/scannerPrograms.java
@@ -0,0 +1,38 @@
+// =========================== SCANNER PROGRAMS ============================
+
+import java.util.Scanner;
+public class scannerPrograms {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+   
+        // boolean a = sc.nextBoolean();
+   
+        // byte b = sc.nextByte();
+   
+        // short c = sc.nextShort();
+   
+        // int d = sc.nextInt();
+   
+        // long e = sc.nextLong();
+   
+        // float f = sc.nextFloat();
+   
+        // double g = sc.nextDouble();
+   
+        // char h = sc.next().charAt(0);
+   
+        // String s = sc.nextLine();
+    
+
+// ============================================================================
+
+                        System.out.print("Enter your name: ");            
+                            String name = sc.nextLine();
+                        System.out.println("your name is :"+name);
+
+// ===============================================================================            
+            
+
+        }
+}

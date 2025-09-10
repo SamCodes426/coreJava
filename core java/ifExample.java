bbeaef682a3a3313dae13880f76f90c8eaba33f9
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-10 13:10:49 +0530

    learn about if statement

---
+++ core java/ifExample.java
@@ -0,0 +1,11 @@
+public class ifExample {
+    public static void main(String[] args) {
+         int k = 5;
+         if (k<10) {
+             System.out.println("HELLO");
+             System.out.println("k is less than 10");
+         }
+         
+         System.out.println("BYE");
+    }    
+}

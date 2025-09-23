5c5ffa2508a618ca2389ecff67d1edef1ad74359
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-23 11:39:11 +0530

    doWhileLoop example

---
+++ core java/doWhileLoop.java
@@ -0,0 +1,10 @@
+public class doWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        do { 
+           System.out.println("hello");
+           i++; 
+        } while (i<=5);
+        System.out.println("bye "+i);    
+    }    
+}

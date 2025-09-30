328c2aecd40c5222e7e5c601f19f6e6b2d787c03
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2025-09-30 11:48:03 +0530

    practise question for method

---
+++ core java/methodPractise.java
@@ -0,0 +1,13 @@
+public class methodPractise {
+    public static void main(String[] args) {
+        addTwoNum(67.0, 2.0);
+        Sqrt(625);
+    }
+
+    public static void addTwoNum(double num1 ,double num2){
+        System.out.println("Sum of two numbers is : "+(num1+num2));
+    }
+    public static void Sqrt(double num){
+        System.out.println("Square root of the number is : "+Math.sqrt(num));
+    }
+}
